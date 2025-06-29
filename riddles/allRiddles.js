5503f1512951c2b11df71c5adac30c66200921f3
elazar matan <elazarmata8@gmail.com>
2025-06-29 09:26:52 +0300

    fix: change riddles declaration from object to array

--- riddles/allRiddles.js
+++ riddles/allRiddles.js
@@ -2,5 +2,5 @@ import r1 from './r1.js'
 import r2 from './r2.js'
 import r3 from './r3.js';
 // import riddle from './../classes/riddle.js'
-const riddles = {r1,r2,r3}
+const riddles = [r1,r2,r3]
 export default riddles;
