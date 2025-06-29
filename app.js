0e0c7627895269fef2a501b76dff89a58af90bee
elazar matan <elazarmata8@gmail.com>
2025-06-29 13:15:15 +0300

    feat: implement player addition functionality with name input

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-import allRiddles from './riddles/allRiddles.js'
+
 import Player from './classes/player.js'
 import game from './game.js'
+import readline from 'readline-sync';
 
-game()
+function addPlayer(){
+    const name = readline.question('whatt your name')
+     const pl = new Player(name)
+     return pl
+}
+const player = addPlayer()
+game(player)
