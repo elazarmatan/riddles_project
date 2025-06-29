5a14440599aed0e77d2e47a4bfc5fdaa15729cad
elazar matan <elazarmata8@gmail.com>
2025-06-29 11:33:08 +0300

    refactor: enhance Riddle class structure and improve ask method output

--- classes/riddle.js
+++ classes/riddle.js
@@ -1,26 +1,28 @@
 import readline from 'readline-sync';
-export default class riddle{
 
-constructor({id,name,taskDescription,correctAnswer}){
-    this.id =id
-    this.name = name
-    this.taskDescription = taskDescription
-    this.correctAnswer = correctAnswer
-}
 
-ask(){
-    console.log(`name: ${this.name}`)
-    console.log(`question : ${this.taskDescription}`)
-    let flag = true
-    while(flag){
-        const res = readline.question();
-        if(res === this.correctAnswer){
-            console.log('correct answer')
-            flag = false
-        }
-        else{
-            console.log('you fail')
-        }
+export default class Riddle {
+  constructor({ id, name, taskDescription, correctAnswer, difficulty, timeLimit }) {
+    this.id = id;
+    this.name = name;
+    this.taskDescription = taskDescription;
+    this.correctAnswer = correctAnswer;
+    this.difficulty = difficulty;
+    this.timeLimit = timeLimit; 
+  }
+
+  ask() {
+    console.log(`Riddle: ${this.name}`);
+    console.log(`${this.taskDescription}`);
+    let flag = true;
+    while (flag) {
+      const res = readline.question('Answer: ');
+      if (res === this.correctAnswer) {
+        console.log('Correct!');
+        flag = false;
+      } else {
+        console.log('Incorrect. Try again.');
+      }
     }
-}
+  }
 }
