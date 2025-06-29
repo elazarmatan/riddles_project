007b255d782dcdd49bce0b86019aade513c80faa
elazar matan <elazarmata8@gmail.com>
2025-06-29 12:36:56 +0300

    feat: add hint functionality to Riddle class with time penalty for usage

--- classes/riddle.js
+++ classes/riddle.js
@@ -2,20 +2,28 @@ import readline from 'readline-sync';
 
 
 export default class Riddle {
-  constructor({ id, name, taskDescription, correctAnswer, difficulty, timeLimit }) {
+  constructor({ id, name, taskDescription, correctAnswer, difficulty, timeLimit ,hint}) {
     this.id = id;
     this.name = name;
     this.taskDescription = taskDescription;
     this.correctAnswer = correctAnswer;
     this.difficulty = difficulty;
     this.timeLimit = timeLimit; 
+    this.hint = hint
   }
 
   ask() {
+    let enter = Date.now()
     console.log(`Riddle: ${this.name}`);
     console.log(`${this.taskDescription}`);
     let flag = true;
+    let addhin = 0
     while (flag) {
+    const hint = readline.question('if you want hint press v but will add you 5 seconds: ');
+    if(hint === "v"){
+        console.log(`the hint: ${this.hint}`)
+        addhin += 5000
+    }
       const res = readline.question('Answer: ');
       if (res === this.correctAnswer) {
         console.log('Correct!');
@@ -24,5 +32,12 @@ export default class Riddle {
         console.log('Incorrect. Try again.');
       }
     }
+    let finish = Date.now()
+    let time = finish - enter 
+        if(time  > this.timeLimit){
+            time += 5000
+            console.log('Too slow! 5 seconds penalty applied')
+        }
+    return time += addhin
   }
 }
