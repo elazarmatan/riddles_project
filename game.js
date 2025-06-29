c215d9cf5ad3430fadd9a829da2c70395b63fa73
elazar matan <elazarmata8@gmail.com>
2025-06-29 09:26:40 +0300

    refactor: streamline game function to use loop for riddles

--- game.js
+++ game.js
@@ -8,24 +8,21 @@ import readline from 'readline-sync';
 export default function game(){
     const name = readline.question('whatt your name')
     const pl = new Player(name)
-    let enter1 = Date.now()
-    const rid1 = new riddle(allriddels.r1)
-    rid1.ask()
-    let finish1 = Date.now()
-    p.recordTime(enter1,finish1)
-
-    let enter2 = Date.now()
-    const rid2 = new riddle(allriddels.r2)
-    rid2.ask()
-    let finish2 = Date.now()
-    p.recordTime(enter2,finish2)
-
-    let enter3 = Date.now()
-    const rid3 = new riddle(allriddels.r3)
-    rid3.ask()
-    let finish3 = Date.now()
-    p.recordTime(enter3,finish3)
-
-    p.showStats()
+    let flag = true
+    let counter = 0
+    while(flag){
+        if (counter === allriddels.length){
+            flag = false
+        }
+        else{
+        let enter1 = Date.now()
+        const rid1 = new riddle(allriddels[counter])
+        rid1.ask()
+        let finish1 = Date.now()
+        pl.recordTime(enter1,finish1)
+        }
+        counter++
+    }
+    pl.showStats()
 }
 
