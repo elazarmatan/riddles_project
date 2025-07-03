e084d93ecf66e76900e592a066f582ac2c34a9d3
elazar matan <elazarmata8@gmail.com>
2025-07-03 13:15:42 +0300

    feat: refactor createLevel function to directly read riddle file

--- game_manager/createLevel.js
+++ game_manager/createLevel.js
@@ -1,20 +1,19 @@
 import {readFile} from 'node:fs/promises'
 import readline from 'readline-sync';
+import {read} from '../crud/read.js'
 
-async function read() {
-    return readFile('./riddles/riddle.txt', 'utf8')
-}
+
 
 
 export async function createLevel(){
     let diffic = readline.question('Choose difficulty: easy / medium / hard: ');
 
     if (diffic !== "easy" && diffic !== "medium" && diffic !== "hard") {
-        game();
+        createLevel()
         return;
     }
 
-    const p1 = await read()
+    const p1 = await read('./riddles/riddle.txt')
     const allriddels = JSON.parse(p1)
     const difarr = allriddels.filter(ch => ch.difficulty === diffic).sort(() => Math.random() - 0.5).slice(0, 5);
     return difarr
