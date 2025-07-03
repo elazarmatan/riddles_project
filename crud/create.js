0db3087aa22ee740b5bbd9a095c11048e21680b3
elazar matan <elazarmata8@gmail.com>
2025-07-03 15:38:29 +0300

    feat: refactor create function to use dataReceived for input and improve error handling

--- crud/create.js
+++ crud/create.js
@@ -1,31 +1,24 @@
 import { writeFile } from 'node:fs/promises'
-import readline from 'readline-sync';
 
 
 
-export function create(path,data){
+export function create(path, dataExists, dataReceived) {
     let newarr
-    try{
-    newarr = JSON.parse(data)
-    }catch(err){
-        console.log("a",err.message)
+    try {
+        ExistsArr = JSON.parse(dataExists)
+    } catch (err) {
+        console.log("a", err.message)
         return
     }
-    const answer = readline.question('what the data: ');
-        let conans
-        try{
-        conans = JSON.parse(answer)
-        }catch(err){
-        console.log("b",err.message)
-        return;
-        }
-        newarr.push(conans)
-        let finishAnswer = JSON.stringify(newarr)
-    writeFile(path,finishAnswer,(err)=> {
-        if(err){
+    const id = (ExistsArr[ExistsArr.length-1]).id
+    const answer = dataReceived(id)
+    ExistsArr.push(answer)
+    let finishData = JSON.stringify(ExistsArr,null,2)
+    writeFile(path, finishData, (err) => {
+        if (err) {
             console.log(err.message)
         }
-        else{
+        else {
             console.log('done')
         }
     })
