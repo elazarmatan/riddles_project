c1f08903f0214e1aeacfcf5e665395cc9fccef3d
elazar matan <elazarmata8@gmail.com>
2025-07-06 16:45:00 +0300

    refactor: simplify data handling in create function by removing unnecessary parsing

--- crud/create.js
+++ crud/create.js
@@ -3,17 +3,11 @@ import { writeFile } from 'node:fs/promises'
 
 
 export function create(path, dataExists, dataReceived) {
-    let newarr
-    try {
-        ExistsArr = JSON.parse(dataExists)
-    } catch (err) {
-        console.log("a", err.message)
-        return
-    }
-    const id = (ExistsArr[ExistsArr.length-1]).id
+    
+    const id = (dataExists[dataExists.length-1]).id
     const answer = dataReceived(id)
-    ExistsArr.push(answer)
-    let finishData = JSON.stringify(ExistsArr,null,2)
+    dataExists.push(answer)
+    let finishData = JSON.stringify(dataExists,null,2)
     writeFile(path, finishData, (err) => {
         if (err) {
             console.log(err.message)
