9f4c1ac5a6bc72cd4df31df76394bca6d04f0ff5
elazar matan <elazarmata8@gmail.com>
2025-07-06 16:45:35 +0300

    fix: correct import path and ensure consistent case handling in menu options

--- game_manager/menu.js
+++ game_manager/menu.js
@@ -1,5 +1,5 @@
 import readline from 'readline-sync';
-import {addPlayer,game} from './game_manager/game.js'
+import {addPlayer,game} from './game.js'
 
 
 export async function menu(){
@@ -13,14 +13,16 @@ export async function menu(){
         '3.exit')
          const choice = readline.question('what your choice')
          switch(choice){
-            case 1:
-            game(player)
+            case '1':
+            await game(player)
             break
-            case 2:
+            case '2':
                 menuCrud()
                 break
-            case 3:
+            case '3':
                 flag = false
+            default:
+                break
          }
     }
 }
