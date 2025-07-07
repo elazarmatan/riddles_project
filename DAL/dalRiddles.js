f927c504cfbcb9a120ddd6c20b013852b860adba
elazar matan <elazarmata8@gmail.com>
2025-07-07 11:53:52 +0300

    This commit introduces a new module, `dalRiddles.js`, responsible for handling data access logic related to riddles. It provides a menu-driven interface for creating, updating, and potentially deleting riddles. The module imports functions for creating and updating data, as well as functions from the `services` directory for the riddle creation and update logic. It also uses `readline-sync` for user input. The main function, `dalRiddles`, presents a menu to the user, allowing them to choose an action. Based on the user's choice, it calls the appropriate functions to create or update riddles, utilizing data read from a file.  The "delete" option is currently a placeholder. The loop continues until the user chooses to exit.

---
+++ DAL/dalRiddles.js
@@ -0,0 +1,37 @@
+import readline from 'readline-sync';
+import {create} from './create.js'
+import {update} from './update.js'
+import {createRiddle} from '../services/createRiddle.js'
+import {updateRiddle} from '../services/updateRiddle.js'
+import {read} from './read.js'
+
+
+export async function dalRiddles() {
+    let exit = true
+    while (exit) {
+        console.log('menu\n' +
+            '1.create new riddle\n' +
+            '2.update riddle exist\n' +
+            '3.delete riddle\n' +
+            '4.exit'
+        )
+        const choice = readline.question('what your choice: ')
+        switch (choice) {
+            case '1':
+                let dataExistC = await read()
+                create('./riddles/riddle.txt',dataExistC,createRiddle)
+                break;
+            case '2':
+                let dataExistU = await read()
+                update('./riddles/riddle.txt',dataExistU,updateRiddle)
+                break
+            case '3':
+                let dataExistD = await read()
+                break
+            case '4':
+                exit = false
+            default:
+                break;
+        }
+    }
+}
