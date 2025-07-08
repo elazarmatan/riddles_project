f3dabd18765bf7083227cabd4e69250d678b6fbb
elazar matan <elazarmata8@gmail.com>
2025-07-08 10:34:17 +0300

    This commit modifies the main menu of the riddles game. It adds a new option, "3. show all players", which allows the user to view a list of all players who have participated in the game. The commit also changes the exit option from "3" to "4" to accommodate the new option. The logic for displaying all players is handled by the `showAllPlayers` function, which reads player data from the `playersDb.txt` file.

--- game_manager/menu.js
+++ game_manager/menu.js
@@ -1,28 +1,35 @@
 import readline from 'readline-sync';
-import {createEventToPlayer,game} from './game.js'
+import { createEventToPlayer, game } from './game.js'
 import { dalRiddles } from '../DAL/dalRiddles.js';
+import {showAllPlayers} from '../services/showAllPlayers.js'
+import {read} from '../DAL/read.js'
 
-export async function menu(){
+export async function menu() {
     console.log('WELCOME TO RIDDLES')
     const player = createEventToPlayer()
     let flag = true
-    while(flag){
-    console.log('menu\n'+
-        '1.new game\n'+
-        '2.crud riddles\n'+
-        '3.exit')
-         const choice = readline.question('what your choice')
-         switch(choice){
+    while (flag) {
+        console.log('menu\n' +
+            '1.new game\n' +
+            '2.crud riddles\n' +
+            '3.show all players\n' +
+            '4.exit')
+        const choice = readline.question('what your choice')
+        switch (choice) {
             case '1':
-            await game(player)
-            break
+                await game(player)
+                break
             case '2':
                 await dalRiddles()
                 break
-            case '3':
+                case '3':
+                    let players = await read('./DAL/playersDb.txt')
+                    showAllPlayers(players)
+                    break
+            case '4':
                 flag = false
             default:
                 break
-         }
+        }
     }
 }
