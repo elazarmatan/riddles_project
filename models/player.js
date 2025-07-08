8d8633951bf288712268d0929dbd5634f0fed72f
elazar matan <elazarmata8@gmail.com>
2025-07-08 10:34:26 +0300

    This commit adds a new method, `getAlltime()`, to the `Player` class. This method calculates and returns the sum of all time values stored in the player's `time` array. This provides a way to directly access the total time without needing to iterate through the array externally.

--- models/player.js
+++ models/player.js
@@ -13,6 +13,14 @@ export default class Player{
         }
         console.log(`stats of player ${this.name}: all time ${(allTime / 1000).toFixed(3)} average ${(allTime / this.time.length/ 1000).toFixed(3)}`)
     }
+    getAlltime(){
+        
+        let allTime = 0
+        for (const element of this.time) {
+            allTime += element
+        }
+        return allTime
+    }
     ResetArray(){
         this.time = []
     }
