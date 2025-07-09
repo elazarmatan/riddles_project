6b7803e3d2717a576551e758b3992c80fc8d776c
elazar matan <elazarmata8@gmail.com>
2025-07-09 17:01:40 +0300

    Refactor: Remove unused parameters from `dataReceived` function in `create` function.
    The `player` and `id` parameters were being passed to the `dataReceived` function, but the function itself was not using them. This commit removes those unused parameters, simplifying the code and potentially preventing confusion. The `dataReceived` function is now called without any arguments.

--- DAL/create.js
+++ DAL/create.js
@@ -5,7 +5,7 @@ import { writeFile } from 'node:fs/promises'
 export async function create(path, dataExists, dataReceived,player) {
     
     const id = (dataExists[dataExists.length-1]).id
-    const answer = dataReceived(id,player)
+    const answer = dataReceived
     dataExists.push(answer)
     let finishData = JSON.stringify(dataExists,null,2)
     await writeFile(path, finishData)
