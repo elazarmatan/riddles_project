27b311725937f792e730b4fd33aa6dd31cf6f9b7
elazar matan <elazarmata8@gmail.com>
2025-07-10 15:50:31 +0300

    transfer

--- services/deleteRiddle.js
+++
@@ -1,8 +0,0 @@
-import readline from 'readline-sync';
-
-export async function deleteRiddle(){
-    const id = readline.question('what the id of riddle do you want to delete: ')
-    await fetch(`http://localhost:2030/riddle/delete/${id}`,{
-        method:'DELETE'
-    })
-}
