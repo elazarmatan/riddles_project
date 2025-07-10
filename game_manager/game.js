fd39217a516d3fcaa1adc4c9cbb538c4a0f99663
elazar matan <elazarmata8@gmail.com>
2025-07-10 12:29:14 +0300

    Refactor game logic and data fetching.
    This commit refactors the `game` function to improve data fetching and player record updates. It introduces the following changes:
    
    - **Fetch Riddles:** Fetches all riddles using the new `getRiddles` function before creating the difficulty array.
    - **Fetch Players:** Fetches all players using the new `getPlayers` function.
    - **Update Player Time:** Updates the player's time using the new `updateTimeToPlayer` function.
    - **Create Player:** Creates a new player using the new `createPlayer` function.
    - **Clean Up:**  Simplifies the logic for updating player records and creating new players, making the code more readable.
    - **Player Stats:** Shows player stats after creating or updating the player.

--- game_manager/game.js
+++ game_manager/game.js
@@ -7,6 +7,7 @@ import {create} from '../DAL/create.js'
 import {checkIfPlayerExist,createPlayer} from '../services/creatPlayer.js'
 import {updateTimeToPlayer} from '../services/updateTimeToPlayer.js'
 import { update } from '../DAL/update.js';
+import {getPlayers,getRiddles} from './fetch.js'
 
 
 //"This function receives a name and creates a Player instance."
@@ -15,6 +16,7 @@ export function createEventToPlayer() {
     const pl = new Player(name);
     return pl;
 }
+
 
 
 // Main game function that runs one round of the riddle game.
@@ -27,7 +29,8 @@ export function createEventToPlayer() {
 //     - Shows total and average time
 // - Asks if the user wants to continue playing.
 export async function game(player) {
-    const difarr = await cr.createLevel()
+    const allRiddles = await getRiddles()
+    const difarr = await cr.createLevel(allRiddles)
     let time 
     for (let i = 0; i < difarr.length; i++) {
         const rid1 = new riddle(difarr[i]);
@@ -43,19 +46,21 @@ export async function game(player) {
 
         player.recordTime(time);
     }
-    let players = await read('../server/db/playersDb.txt')
+    let players = await getPlayers()
     const idx = players.findIndex(pl => pl.name === player.name)
-    
+    const allTime = player.getAlltime()
     if(checkIfPlayerExist(players,player.name)){
-        if(players[idx].time > player. getAlltime()){
+        if(players[idx].time > allTime){
             console.log(`\nCongratulations ${player.name} You broke your own record\n`)
-            await update('../server/db/playersDb.txt',players,updateTimeToPlayer,player.getAlltime(),idx)
+            player.showStats()
+            await updateTimeToPlayer(player,allTime,idx)
         }
     }
     else{
-        await create('../server/db/playersDb.txt',players,createPlayer,player)
+        await createPlayer(player)
+        player.showStats()
     }
-    player.showStats();
+    
     player.ResetArray();
 
     const continu = readline.question('\nIf you want to continue, press v. Otherwise, enter any key: ');
