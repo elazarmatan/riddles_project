f547b5a316d4bbe9a55b7f1e9e8526f0216b41e8
elazar matan <elazarmata8@gmail.com>
2025-07-10 12:29:51 +0300

    Refactor: Move riddle filtering logic to the caller.
    This commit moves the responsibility of reading and filtering riddles from the `createLevel` function to the caller.  Instead of `createLevel` reading the riddle database itself, it now accepts a pre-filtered array of riddles (`allriddels`) as an argument.  This improves the function's reusability and allows for greater control over how the riddle data is sourced and processed. The `read` operation is now handled outside this function and the filtering logic is now external.

--- services/createLevel.js
+++ services/createLevel.js
@@ -4,12 +4,11 @@ import { read } from '../DAL/read.js'
 
 
 
-export async function createLevel() {
+export async function createLevel(allriddels) {
     let flag = true
     while (flag) {
         let diffic = readline.question('Choose difficulty: easy / medium / hard: ');
         if (diffic === "easy" || diffic === "medium" || diffic === "hard") {
-            const allriddels = await read('../server/db/riddle.txt')
             const difarr = allriddels.filter(ch => ch.difficulty === diffic).sort(() => Math.random() - 0.5).slice(0, 5);
             return difarr
         }
