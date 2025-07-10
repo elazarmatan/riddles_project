05cc15b010871a6f6b5d6dde34eda896e43d187c
elazar matan <elazarmata8@gmail.com>
2025-07-10 12:30:11 +0300

    Refactor: Update riddle service to use API call
    The `updateRiddle` function now calls the API to update a riddle instead of directly modifying an array of objects.
    The function now takes no arguments.
    It prompts the user for the ID of the riddle to update, the property to change, and the new value for the property.
    It then makes a PUT request to the `/riddle/update/:id` endpoint with the changes and property in the request body.

--- services/updateRiddle.js
+++ services/updateRiddle.js
@@ -1,12 +1,16 @@
 import readline from 'readline-sync';
 
 
-export function updateRiddle(arrObj){
+export async function updateRiddle(){
     const id = readline.question('what the id of riddle do you want to change: ')
     const property = readline.question('what do you want to change: ')
     const change = readline.question('what the change do yo want to push: ')
-    const idx = arrObj.findIndex(obj => obj.id === parseInt(id, 10))
-    arrObj[idx][property] = change
-    return arrObj
+    await fetch(`http://localhost:2030/riddle/update/${id}`,{
+        method:'PUT',
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({"changes":change,"property":property})
+    })
 }
 
