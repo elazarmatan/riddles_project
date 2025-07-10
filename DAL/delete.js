ec55d3cf9101ccd6f6e7f9aaa88db446f296bafd
elazar matan <elazarmata8@gmail.com>
2025-07-10 13:33:14 +0300

    This commit introduces a new file `DAL/delete.js` containing a function `Delete`. This function takes a file path (`path`), an index (`idx`), and an array of data (`dataExist`) as input. It removes the element at the specified index from the `dataExist` array using `splice`. Finally, it writes the modified `dataExist` array back to the file at the given `path` as a JSON string, formatted with indentation for readability. This function effectively deletes an element from a JSON file based on its index in the array stored within the file.

---
+++ DAL/delete.js
@@ -0,0 +1,9 @@
+import {writeFile} from 'node:fs/promises'
+
+
+export async function Delete(path, idx, dataExist) {
+    dataExist.splice(idx, 1)
+    await writeFile(path, JSON.stringify(dataExist, null, 2))
+}
+
+
