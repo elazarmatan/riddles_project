0ca5786ced1da4e92cfacc394e86530f9c265f00
elazar matan <elazarmata8@gmail.com>
2025-07-15 09:05:10 +0300

    error handling

--- server/DAL/create.js
+++ server/DAL/create.js
@@ -3,12 +3,15 @@ import { writeFile } from 'node:fs/promises'
 
 
 export async function create(path, dataExists, dataReceived) {
-    
-    
     const answer = dataReceived
     dataExists.push(answer)
     let finishData = JSON.stringify(dataExists,null,2)
+    try{
     await writeFile(path, finishData)
     console.log('cretad successfully!!!!')
+    }
+    catch(err){
+        console.error(err)
+    }
 }
 
--- server/DAL/read.js
+++ server/DAL/read.js
@@ -2,6 +2,11 @@ import {readFile} from 'node:fs/promises'
 
 
 export async function read(path) {
+    try{
     const res = await readFile(path, 'utf8')
     return JSON.parse(res)
+    }
+    catch(err){
+        console.error(err)
+    }
 }
