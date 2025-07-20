8aa7c6f7fcb9d8b178787c1526485182fc57145e
elazar matan <elazarmata8@gmail.com>
2025-07-20 12:28:35 +0300

    This commit modifies the `dallRiddles.js` file, which handles data access for riddles in the application. The changes include:
    -   Adding imports: Imports `ObjectId` from 'mongodb' to handle database IDs.
    -   Adding create riddle: added the function to create a riddle
    -   Adding get all riddles: added the function to get all the riddles.
    -   Adding delete riddle: added the function to delete a specific riddle by id.
    -   Adding update riddle: added the function to update a specific riddle by id, property and new value.
    -   Adding get riddles by level: added the function to get all the riddles that have the same level.
    -   Adding get riddles by id: added the function to get a specific riddle by id.
    -   Adding console logs: added a few console logs to check if the functions work.

--- server/DAL/dallRiddles.js
+++ server/DAL/dallRiddles.js
@@ -1,24 +1,41 @@
 import "dotenv/config"
 import { mongoClient } from '../db/mongoDb.js'
+import { ObjectId } from 'mongodb'
 
 const db = mongoClient.db('riddle_project')
 
 export async function createRiddle(riddle) {
-    await db.collection('riddles').insertOne(riddle)
+   await db.collection('riddles').insertOne(riddle)
+   
 }
 
 export async function getAllRiddle() {
-
+    const data = await db.collection('riddles').find().toArray()
+    return data
 }
 
-export async function deleteRiddle() {
-
+export async function deleteRiddle(id) {
+    await db.collection('riddles').deleteOne({ _id: new ObjectId(id) });
 }
 
-export async function updateRiddle() {
-
+export async function updateRiddle(property,newValue,id) {
+    await db.collection('riddles').updateOne({ _id: new ObjectId(id) }, { $set: {[property]: newValue} } );
 }
 
-export async function getRiddlesByLevel() {
-
+export async function getRiddlesByLevel(specficLevel) {
+    const level = await db.collection('riddles').find({difficulty:specficLevel}).toArray();
+    return level
 }
+
+
+export async function getRiddlesById(id) {
+    const counter = await db.collection('riddles').find({_id:new ObjectId(id)}).toArray();
+    return counter
+}
+// const r = await getAllRiddle()
+// console.log(r)
+// createRiddle({name:"david",level:'easy'})
+// await deleteRiddle('687c8da2ef1f18b44119d2b0')
+// const level = await getRiddlesByLevel("easy")
+// console.log(level)
+// await updateRiddle('name','moshe','687c8de4046a44f5fc145ac8')
