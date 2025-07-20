a0ec5042c5cf0e0852bb5c33638015c34b9908ec
elazar matan <elazarmata8@gmail.com>
2025-07-20 12:28:48 +0300

    Refactor riddles route to use DAL functions
    This commit refactors the riddles route to use the new DAL functions for creating, updating, deleting, and retrieving riddles. It also removes the old read, create, update, and delete functions.
    
    The following changes were made:
    
    - Replaced the old read, create, update, and delete functions with the new DAL functions.
    - Updated the update route to use the updateRiddle DAL function.
    - Updated the create route to use the createRiddle DAL function.
    - Updated the delete route to use the deleteRiddle DAL function.
    - Added a new route to get riddles by level using the getRiddlesByLevel DAL function.
    - Updated the getall route to use the getAllRiddle DAL function.

--- server/routes/riddles.js
+++ server/routes/riddles.js
@@ -1,9 +1,5 @@
 import express from 'express'
-import { read } from '../DAL/read.js'
-import { create } from '../DAL/create.js'
-import { update } from '../DAL/update.js'
-import { Delete } from '../DAL/delete.js'
-import { createRiddle } from '../DAL/dallRiddles.js'
+import { createRiddle ,updateRiddle,deleteRiddle,getAllRiddle,getRiddlesByLevel,getRiddlesById} from '../DAL/dallRiddles.js'
 
 
 const riddlesDbPath = '../server/db/riddle.txt'
@@ -11,10 +7,8 @@ const riddlesDbPath = '../server/db/riddle.txt'
 const router = express.Router()
 // const path = '../server/db/riddle.txt'
 router.put('/update/:id', async (req, res) => {
-    const data = await read(riddlesDbPath)
-    const idx = data.findIndex(riddle => riddle.id === parseInt(req.params.id))
     try {
-        await update(riddlesDbPath, req.body.changes, data, idx, req.body.property)
+        await updateRiddle(req.body.property,req.body.changes,req.params.id)
         res.json({ msg: 'succes' })
     } catch (err) {
         console.error(' Error during update:', err)
@@ -24,9 +18,9 @@ router.put('/update/:id', async (req, res) => {
 })
 
 router.post('/create', async (req, res) => {
-    // const data = await read(riddlesDbPath)
-    // const id = (data[data.length - 1]).id
-    // req.body.id = id + 1
+    const id = await getRiddlesById('687cac7422a52d221c8b84a1')
+    req.body._id = parseInt(id[0].counter)+ 1;
+    await updateRiddle('counter', parseInt(id[0].counter)+ 1,'687cac7422a52d221c8b84a1')
     try {
         await createRiddle(req.body)
         res.json({ msg: 'succes' })
@@ -38,7 +32,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/getall', async (req, res) => {
     try {
-        const data = await read(riddlesDbPath)
+        const data = await getAllRiddle()
         res.json(data)
     }
     catch (err) {
@@ -48,10 +42,25 @@ router.get('/getall', async (req, res) => {
 })
 
 router.delete('/delete/:id', async (req, res) => {
-    const data = await read(riddlesDbPath)
-    const idx = data.findIndex(riddle => riddle.id === parseInt(req.params.id))
-    await Delete(riddlesDbPath, idx, data)
-    res.end()
+    try {
+       await deleteRiddle(req.params.id)
+        res.json({ msg: 'succes' })
+    }
+    catch (err) {
+        res.status(400)
+        res.end(err)
+    }
+})
+
+router.get('/getByLevel/:level', async (req, res) => {
+    try {
+        const data = await getRiddlesByLevel(req.params.level)
+        res.json(data)
+    }
+    catch (err) {
+        res.status(400)
+        res.end(err)
+    }
 })
 
 export default router
