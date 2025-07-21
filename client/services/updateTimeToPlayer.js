b8f2490debc3fa9d363214ed7656622362802ced
elazar matan <elazarmata8@gmail.com>
2025-07-21 12:09:11 +0300

    Refactor: Update `updateTimeToPlayer` to use player name instead of index and add error handling.
    This commit changes the `updateTimeToPlayer` function to identify players by name instead of index when updating their time on the server.  It also adds error handling to catch and report server-side errors during the update process. The function now returns the JSON response from the server.

--- client/services/updateTimeToPlayer.js
+++ client/services/updateTimeToPlayer.js
@@ -1,9 +1,14 @@
-export async function updateTimeToPlayer(time,idx){
-    await fetch(`http://localhost:2030/player/update/${idx}`,{
+export async function updateTimeToPlayer(time,name){
+    const res = await fetch(`http://localhost:2030/player/update/${name}`,{
         method:'PUT',
         headers:{
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({"time":time,"property":"time"})
     })
+    if (!res.ok) {
+        const errorText = await res.text(); // נסה לקרוא טקסט במקום JSON
+        throw new Error(`Server error: ${res.status} - ${errorText}`);
+    }
+    return await res.json()
 }
