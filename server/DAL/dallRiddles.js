c322da4e8c2587c0051dc9d8409c7e91f88bb322
elazar matan <elazarmata8@gmail.com>
2025-07-21 12:09:17 +0300

    Refactor riddle ID handling in database operations
    This commit modifies the `deleteRiddle` and `updateRiddle` functions in `dallRiddles.js` to use `riddle_id` for identifying riddles instead of `_id`. It also adds a new function `updateCounter` that uses `_id` to update specific properties of riddles. Error handling (try-catch blocks) has been added to `updateRiddle` and `updateCounter` to catch potential database errors and log them to the console.

--- server/DAL/dallRiddles.js
+++ server/DAL/dallRiddles.js
@@ -14,11 +14,28 @@ export async function getAllRiddle() {
 }
 
 export async function deleteRiddle(id) {
-    await db.collection('riddles').deleteOne({ _id: new ObjectId(id) });
+    await db.collection('riddles').deleteOne({ riddle_id:parseInt(id)});
 }
 
 export async function updateRiddle(property,newValue,id) {
-    await db.collection('riddles').updateOne({ _id: new ObjectId(id) }, { $set: {[property]: newValue} } );
+    try{
+    await db.collection('riddles').updateOne({ riddle_id:parseInt(id) }, { $set: {[property]: newValue} } );
+    console.log('succes')
+    }
+    catch(err){
+        console.error('error',err)
+    }
+}
+
+export async function updateCounter(property,newValue,id){
+    try{
+     await db.collection('riddles').updateOne({_id:new ObjectId(id)}, { $set: {[property]: newValue} } );
+     console.log('succes')
+    }
+    catch(err){
+        console.error('error',err)
+    }
+
 }
 
 export async function getRiddlesByLevel(specficLevel) {
@@ -31,10 +48,3 @@ export async function getRiddlesById(id) {
     const counter = await db.collection('riddles').find({_id:new ObjectId(id)}).toArray();
     return counter
 }
-// const r = await getAllRiddle()
-// console.log(r)
-// createRiddle({name:"david",level:'easy'})
-// await deleteRiddle('687c8da2ef1f18b44119d2b0')
-// const level = await getRiddlesByLevel("easy")
-// console.log(level)
-// await updateRiddle('name','moshe','687c8de4046a44f5fc145ac8')
