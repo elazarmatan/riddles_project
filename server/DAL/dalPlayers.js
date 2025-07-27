48170df94a33aca021fec968721d49975da1579c
elazar matan <elazarmata8@gmail.com>
2025-07-27 11:33:53 +0300

    This commit reformats the `dalPlayers.js` file to improve readability. It mainly involves adding spaces around operators and after commas in function calls and object literals. No functional changes are introduced.

--- server/DAL/dalPlayers.js
+++ server/DAL/dalPlayers.js
@@ -3,30 +3,30 @@ import 'dotenv/config'
 
 
 const supabase = createClient(
-   process.env.SUPABASE_URL,
-   process.env.SUPABASE_KEY
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
 )
 
-export async function getAllPlayers(){
-    const {data,error} = await supabase.from('players').select('*')
-    if(error) throw new Error(error.message)
-    return data 
+export async function getAllPlayers() {
+    const { data, error } = await supabase.from('players').select('*')
+    if (error) throw new Error(error.message)
+    return data
 }
 
-export async function create(player){
-    const {data,error} = await supabase.from('players').insert(player)
-    if(error) throw new Error(error.message)
-    return data 
+export async function create(player) {
+    const { data, error } = await supabase.from('players').insert(player)
+    if (error) throw new Error(error.message)
+    return data
 }
 
-export async function update(name,property,value){
-    const {data,error} = await supabase.from('players').update({[property]:value}).eq('name',name)
-    if(error) throw new Error(error.message)
-    return data 
+export async function update(name, property, value) {
+    const { data, error } = await supabase.from('players').update({ [property]: value }).eq('name', name)
+    if (error) throw new Error(error.message)
+    return data
 }
 
-export async function checkIfPlayerExist(playerName){
-    const {data,error} = await supabase.from('players').select('*').eq('name',playerName)
-    if(error) throw new Error(error.message)
-    return data 
+export async function checkIfPlayerExist(playerName) {
+    const { data, error } = await supabase.from('players').select('*').eq('name', playerName)
+    if (error) throw new Error(error.message)
+    return data
 }
