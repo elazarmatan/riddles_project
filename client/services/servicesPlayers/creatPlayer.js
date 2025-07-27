6b7e6806300851b4b3e754b5ece9864af54f6606
elazar matan <elazarmata8@gmail.com>
2025-07-27 11:32:59 +0300

    Refactor player creation and add login functionality.
    This commit introduces several changes to the player creation service:
    
    -   Removes the `PlayerName` and `playerTime` parameters from the `createPlayer` function.
    -   Adds a dependency on the `readline-sync` package for interactive input.
    -   Modifies the `createPlayer` function to prompt the user for their name and password and creates a new player object.
    -   Adds a `loginPlayer` function that prompts the user for their name and password and attempts to log them in.
    -   Removes the `checkIfPlayerExist` function.
    -   Adds a `checkBrokeRecord` function to check if the player has broken their record and update the time.

--- client/services/servicesPlayers/creatPlayer.js
+++ client/services/servicesPlayers/creatPlayer.js
@@ -1,34 +1,67 @@
-export async function createPlayer(PlayerName,playerTime) {
+import readline from 'readline-sync';
+import { updateTimeToPlayer } from './updateTimeToPlayer.js'
+
+
+export async function createPlayer() {
     let player = {}
-    player.name = PlayerName
-    player.time = playerTime
-    const res = await fetch('http://localhost:2030/player/create ',{
+    player.name = readline.question('What is your name: ');
+    player.password = readline.question('What is your password: ');
+    player.role = 'user'
+    const res = await fetch('http://localhost:2030/player/create ', {
         method: 'POST',
         body: JSON.stringify(player),
         headers: {
             "Content-Type": "application/json"
         }
     })
+    if(res.status === 400) return 'err'
+    return await res.json()
+}
+
+export async function loginPlayer() {
+    let player = {}
+    const name = readline.question('What is your name: ');
+    const password = readline.question('What is your password: ');
+    player.name = name
+    player.password = password
+    const res = await fetch('http://localhost:2030/player/login ', {
+        method: 'POST',
+        body: JSON.stringify(player),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    if(res.status === 403) return 'err'
     return await res.json()
 }
 
 
 
-export async function checkIfPlayerExist(name) {
-    const res = await fetch(`http://localhost:2030/player/getByName/${name}`)
-    const exist = await res.json()
-    if(exist.length > 0){
-        return true
-    }
-    else{
-        return false
-    }
-}
 
 
 
-export async function getTimeToPlayer(name){
+
+export async function getTimeToPlayer(name) {
     const time = await fetch(`http://localhost:2030/player/getByName/${name}`)
     const finishTime = await time.json()
     return finishTime[0].time
+}
+
+
+export async function checkBrokeRecord(player) {
+    const allTime = player.getAlltime()
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    const playerRecord = await getTimeToPlayer(player.name)
+    if (playerRecord !== null) {
+        if (playerRecord > allTime) {
+            console.log(`\nCongratulations ${player.name} You broke your own record\n`)
+            await new Promise(resolve => setTimeout(resolve, 1000))
+            const update = await updateTimeToPlayer(allTime, player.name)
+            console.log(update)
+        }
+    }
+    else {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        const update = await updateTimeToPlayer(allTime, player.name)
+    }
 }
