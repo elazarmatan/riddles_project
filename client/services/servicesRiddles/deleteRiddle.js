9623b6c1933c9702eb09b2c865bc422c080128d9
elazar matan <elazarmata8@gmail.com>
2025-07-27 11:33:21 +0300

    This commit modifies the `deleteRiddle` function in `client/services/servicesRiddles/deleteRiddle.js` to include an authorization token in the request headers. This token is passed as an argument to the function and included within the `cookies` header when making the DELETE request to the server. This likely aims to secure the delete riddle functionality by ensuring only authorized users can delete riddles.

--- client/services/servicesRiddles/deleteRiddle.js
+++ client/services/servicesRiddles/deleteRiddle.js
@@ -1,8 +1,11 @@
 import readline from 'readline-sync';
 
-export async function deleteRiddle(){
+export async function deleteRiddle(token){
     const id = readline.question('what the id of riddle do you want to delete: ')
     await fetch(`http://localhost:2030/riddle/delete/${id}`,{
+         headers: {
+            'cookies':token,
+        },
         method:'DELETE'
     })
 }
